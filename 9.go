9d1d4757472d1d36d67855e49ea6a8af452a43a6
Sanderson Machado <sandersonoficial10@hotmail.com>
2023-06-13 09:38:25 -0300

    oi

--- 9.go
+++ 9.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var s, letra1, letra2 string
+	fmt.Println("Escreva uma string")
+	fmt.Scanln(&s)
+	fmt.Println("Escreva uma letra antiga")
+	fmt.Scanln(&letra1)
+	fmt.Println("Escreva a nova letra")
+	fmt.Scanln(&letra2)
+	s = strings.Replace(s, string(letra1), string(letra2), -1)
+	fmt.Println(s)
 }
